711efacd383c19e5a0776c6e69ffd61603fd8778
MatthewBJCDawkins <mdawkins019@gmail.com>
2024-04-28 15:53:39 +0100

    Add project files.

---
+++ actual workout tracker starter project/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace actual_workout_tracker_starter_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bench = new System.Windows.Forms.Label();
+            this.Squat = new System.Windows.Forms.Label();
+            this.Deadlift = new System.Windows.Forms.Label();
+            this.BBrow = new System.Windows.Forms.Label();
+            this.curl = new System.Windows.Forms.Label();
+            this.Weight = new System.Windows.Forms.Label();
+            this.OHP = new System.Windows.Forms.Label();
+            this.orm = new System.Windows.Forms.Label();
+            this.Reps = new System.Windows.Forms.Label();
+            this.bweight = new System.Windows.Forms.TextBox();
+            this.sweight = new System.Windows.Forms.TextBox();
+            this.dweight = new System.Windows.Forms.TextBox();
+            this.brweight = new System.Windows.Forms.TextBox();
+            this.cweight = new System.Windows.Forms.TextBox();
+            this.oweight = new System.Windows.Forms.TextBox();
+            this.breps = new System.Windows.Forms.TextBox();
+            this.sreps = new System.Windows.Forms.TextBox();
+            this.dreps = new System.Windows.Forms.TextBox();
+            this.brreps = new System.Windows.Forms.TextBox();
+            this.creps = new System.Windows.Forms.TextBox();
+            this.oreps = new System.Windows.Forms.TextBox();
+            this.bm = new System.Windows.Forms.TextBox();
+            this.sm = new System.Windows.Forms.TextBox();
+            this.dm = new System.Windows.Forms.TextBox();
+            this.brm = new System.Windows.Forms.TextBox();
+            this.cm = new System.Windows.Forms.TextBox();
+            this.om = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Bench
+            // 
+            this.Bench.AutoSize = true;
+            this.Bench.Location = new System.Drawing.Point(58, 43);
+            this.Bench.Name = "Bench";
+            this.Bench.Size = new System.Drawing.Size(45, 16);
+            this.Bench.TabIndex = 0;
+            this.Bench.Text = "Bench";
+            this.Bench.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Squat
+            // 
+            this.Squat.AutoSize = true;
+            this.Squat.Location = new System.Drawing.Point(61, 109);
+            this.Squat.Name = "Squat";
+            this.Squat.Size = new System.Drawing.Size(42, 16);
+            this.Squat.TabIndex = 1;
+            this.Squat.Text = "Squat";
+            // 
+            // Deadlift
+            // 
+            this.Deadlift.AutoSize = true;
+            this.Deadlift.Location = new System.Drawing.Point(50, 183);
+            this.Deadlift.Name = "Deadlift";
+            this.Deadlift.Size = new System.Drawing.Size(53, 16);
+            this.Deadlift.TabIndex = 2;
+            this.Deadlift.Text = "Deadlift";
+            // 
+            // BBrow
+            // 
+            this.BBrow.AutoSize = true;
+            this.BBrow.Location = new System.Drawing.Point(23, 249);
+            this.BBrow.Name = "BBrow";
+            this.BBrow.Size = new System.Drawing.Size(80, 16);
+            this.BBrow.TabIndex = 3;
+            this.BBrow.Text = "Barbell Row";
+            // 
+            // curl
+            // 
+            this.curl.AutoSize = true;
+            this.curl.Location = new System.Drawing.Point(35, 319);
+            this.curl.Name = "curl";
+            this.curl.Size = new System.Drawing.Size(68, 16);
+            this.curl.TabIndex = 4;
+            this.curl.Text = "Bicep Curl";
+            // 
+            // Weight
+            // 
+            this.Weight.AutoSize = true;
+            this.Weight.Location = new System.Drawing.Point(134, 19);
+            this.Weight.Name = "Weight";
+            this.Weight.Size = new System.Drawing.Size(49, 16);
+            this.Weight.TabIndex = 5;
+            this.Weight.Text = "Weight";
+            // 
+            // OHP
+            // 
+            this.OHP.AutoSize = true;
+            this.OHP.Location = new System.Drawing.Point(5, 382);
+            this.OHP.Name = "OHP";
+            this.OHP.Size = new System.Drawing.Size(104, 16);
+            this.OHP.TabIndex = 6;
+            this.OHP.Text = "Overhead press";
+            // 
+            // orm
+            // 
+            this.orm.AutoSize = true;
+            this.orm.Location = new System.Drawing.Point(350, 19);
+            this.orm.Name = "orm";
+            this.orm.Size = new System.Drawing.Size(35, 16);
+            this.orm.TabIndex = 7;
+            this.orm.Text = "1RM";
+            // 
+            // Reps
+            // 
+            this.Reps.AutoSize = true;
+            this.Reps.Location = new System.Drawing.Point(249, 19);
+            this.Reps.Name = "Reps";
+            this.Reps.Size = new System.Drawing.Size(40, 16);
+            this.Reps.TabIndex = 8;
+            this.Reps.Text = "Reps";
+            // 
+            // bweight
+            // 
+            this.bweight.Location = new System.Drawing.Point(132, 42);
+            this.bweight.Name = "bweight";
+            this.bweight.Size = new System.Drawing.Size(50, 22);
+            this.bweight.TabIndex = 9;
+            this.bweight.TextChanged += new System.EventHandler(this.bweight_TextChanged);
+            // 
+            // sweight
+            // 
+            this.sweight.Location = new System.Drawing.Point(132, 103);
+            this.sweight.Name = "sweight";
+            this.sweight.Size = new System.Drawing.Size(50, 22);
+            this.sweight.TabIndex = 10;
+            this.sweight.TextChanged += new System.EventHandler(this.sweight_TextChanged);
+            // 
+            // dweight
+            // 
+            this.dweight.Location = new System.Drawing.Point(132, 177);
+            this.dweight.Name = "dweight";
+            this.dweight.Size = new System.Drawing.Size(50, 22);
+            this.dweight.TabIndex = 11;
+            this.dweight.TextChanged += new System.EventHandler(this.dweight_TextChanged);
+            // 
+            // brweight
+            // 
+            this.brweight.Location = new System.Drawing.Point(132, 243);
+            this.brweight.Name = "brweight";
+            this.brweight.Size = new System.Drawing.Size(50, 22);
+            this.brweight.TabIndex = 12;
+            this.brweight.TextChanged += new System.EventHandler(this.brweight_TextChanged);
+            // 
+            // cweight
+            // 
+            this.cweight.Location = new System.Drawing.Point(132, 313);
+            this.cweight.Name = "cweight";
+            this.cweight.Size = new System.Drawing.Size(50, 22);
+            this.cweight.TabIndex = 13;
+            this.cweight.TextChanged += new System.EventHandler(this.cweight_TextChanged);
+            // 
+            // oweight
+            // 
+            this.oweight.Location = new System.Drawing.Point(132, 376);
+            this.oweight.Name = "oweight";
+            this.oweight.Size = new System.Drawing.Size(50, 22);
+            this.oweight.TabIndex = 14;
+            this.oweight.TextChanged += new System.EventHandler(this.oweight_TextChanged);
+            // 
+            // breps
+            // 
+            this.breps.Location = new System.Drawing.Point(239, 42);
+            this.breps.Name = "breps";
+            this.breps.Size = new System.Drawing.Size(50, 22);
+            this.breps.TabIndex = 15;
+            this.breps.TextChanged += new System.EventHandler(this.breps_TextChanged);
+            // 
+            // sreps
+            // 
+            this.sreps.Location = new System.Drawing.Point(239, 103);
+            this.sreps.Name = "sreps";
+            this.sreps.Size = new System.Drawing.Size(50, 22);
+            this.sreps.TabIndex = 16;
+            this.sreps.TextChanged += new System.EventHandler(this.sreps_TextChanged);
+            // 
+            // dreps
+            // 
+            this.dreps.Location = new System.Drawing.Point(239, 177);
+            this.dreps.Name = "dreps";
+            this.dreps.Size = new System.Drawing.Size(50, 22);
+            this.dreps.TabIndex = 17;
+            this.dreps.TextChanged += new System.EventHandler(this.dreps_TextChanged);
+            // 
+            // brreps
+            // 
+            this.brreps.Location = new System.Drawing.Point(239, 243);
+            this.brreps.Name = "brreps";
+            this.brreps.Size = new System.Drawing.Size(50, 22);
+            this.brreps.TabIndex = 18;
+            this.brreps.TextChanged += new System.EventHandler(this.brreps_TextChanged);
+            // 
+            // creps
+            // 
+            this.creps.Location = new System.Drawing.Point(239, 313);
+            this.creps.Name = "creps";
+            this.creps.Size = new System.Drawing.Size(50, 22);
+            this.creps.TabIndex = 19;
+            this.creps.TextChanged += new System.EventHandler(this.creps_TextChanged);
+            // 
+            // oreps
+            // 
+            this.oreps.Location = new System.Drawing.Point(239, 376);
+            this.oreps.Name = "oreps";
+            this.oreps.Size = new System.Drawing.Size(50, 22);
+            this.oreps.TabIndex = 20;
+            this.oreps.TextChanged += new System.EventHandler(this.oreps_TextChanged);
+            // 
+            // bm
+            // 
+            this.bm.Location = new System.Drawing.Point(344, 42);
+            this.bm.Name = "bm";
+            this.bm.ReadOnly = true;
+            this.bm.Size = new System.Drawing.Size(50, 22);
+            this.bm.TabIndex = 21;
+            this.bm.TextChanged += new System.EventHandler(this.textBox13_TextChanged);
+            // 
+            // sm
+            // 
+            this.sm.Location = new System.Drawing.Point(344, 103);
+            this.sm.Name = "sm";
+            this.sm.ReadOnly = true;
+            this.sm.Size = new System.Drawing.Size(50, 22);
+            this.sm.TabIndex = 22;
+            // 
+            // dm
+            // 
+            this.dm.Location = new System.Drawing.Point(344, 177);
+            this.dm.Name = "dm";
+            this.dm.ReadOnly = true;
+            this.dm.Size = new System.Drawing.Size(50, 22);
+            this.dm.TabIndex = 23;
+            // 
+            // brm
+            // 
+            this.brm.Location = new System.Drawing.Point(344, 243);
+            this.brm.Name = "brm";
+            this.brm.ReadOnly = true;
+            this.brm.Size = new System.Drawing.Size(50, 22);
+            this.brm.TabIndex = 24;
+            // 
+            // cm
+            // 
+            this.cm.Location = new System.Drawing.Point(344, 313);
+            this.cm.Name = "cm";
+            this.cm.ReadOnly = true;
+            this.cm.Size = new System.Drawing.Size(50, 22);
+            this.cm.TabIndex = 25;
+            // 
+            // om
+            // 
+            this.om.Location = new System.Drawing.Point(344, 376);
+            this.om.Name = "om";
+            this.om.ReadOnly = true;
+            this.om.Size = new System.Drawing.Size(50, 22);
+            this.om.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(499, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 92);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "calculate one rep max estimates";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bm);
+            this.Controls.Add(this.om);
+            this.Controls.Add(this.cm);
+            this.Controls.Add(this.brm);
+            this.Controls.Add(this.dm);
+            this.Controls.Add(this.sm);
+            this.Controls.Add(this.oreps);
+            this.Controls.Add(this.creps);
+            this.Controls.Add(this.brreps);
+            this.Controls.Add(this.dreps);
+            this.Controls.Add(this.sreps);
+            this.Controls.Add(this.breps);
+            this.Controls.Add(this.oweight);
+            this.Controls.Add(this.cweight);
+            this.Controls.Add(this.brweight);
+            this.Controls.Add(this.dweight);
+            this.Controls.Add(this.sweight);
+            this.Controls.Add(this.bweight);
+            this.Controls.Add(this.Reps);
+            this.Controls.Add(this.orm);
+            this.Controls.Add(this.OHP);
+            this.Controls.Add(this.Weight);
+            this.Controls.Add(this.curl);
+            this.Controls.Add(this.BBrow);
+            this.Controls.Add(this.Deadlift);
+            this.Controls.Add(this.Squat);
+            this.Controls.Add(this.Bench);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Bench;
+        private System.Windows.Forms.Label Squat;
+        private System.Windows.Forms.Label Deadlift;
+        private System.Windows.Forms.Label BBrow;
+        private System.Windows.Forms.Label curl;
+        private System.Windows.Forms.Label Weight;
+        private System.Windows.Forms.Label OHP;
+        private System.Windows.Forms.Label orm;
+        private System.Windows.Forms.Label Reps;
+        private System.Windows.Forms.TextBox bweight;
+        private System.Windows.Forms.TextBox sweight;
+        private System.Windows.Forms.TextBox dweight;
+        private System.Windows.Forms.TextBox brweight;
+        private System.Windows.Forms.TextBox cweight;
+        private System.Windows.Forms.TextBox oweight;
+        private System.Windows.Forms.TextBox breps;
+        private System.Windows.Forms.TextBox sreps;
+        private System.Windows.Forms.TextBox dreps;
+        private System.Windows.Forms.TextBox brreps;
+        private System.Windows.Forms.TextBox creps;
+        private System.Windows.Forms.TextBox oreps;
+        private System.Windows.Forms.TextBox bm;
+        private System.Windows.Forms.TextBox sm;
+        private System.Windows.Forms.TextBox dm;
+        private System.Windows.Forms.TextBox brm;
+        private System.Windows.Forms.TextBox cm;
+        private System.Windows.Forms.TextBox om;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ actual workout tracker starter project/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace actual_workout_tracker_starter_project
+{
+    
+    public partial class Form1 : Form
+    {
+        
+
+        exercise bench = new exercise("bench", 0, 0, 0);
+        exercise squat = new exercise("squat", 0, 0, 0);
+        exercise deadlift = new exercise("deadlift",0,0, 0);
+        exercise barbellrow = new exercise("barbellrow", 0, 0, 0);
+        exercise bicepcurl = new exercise("bicepcurl", 0,0, 0); 
+        exercise ohp = new exercise("ohp",0,0,0);
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void bweight_TextChanged(object sender, EventArgs e)
+        {
+            bench.weight = Convert.ToDouble(bweight.Text);
+        }
+
+        private void breps_TextChanged(object sender, EventArgs e)
+        {
+            bench.reps = Convert.ToDouble(breps.Text);
+        }
+
+        private void sweight_TextChanged(object sender, EventArgs e)
+        {
+            squat.weight = Convert.ToDouble(sweight.Text);
+        }
+
+        private void sreps_TextChanged(object sender, EventArgs e)
+        {
+            squat.reps = Convert.ToDouble(sreps.Text);
+        }
+
+        private void dweight_TextChanged(object sender, EventArgs e)
+        {
+            deadlift.weight = Convert.ToDouble(dweight.Text);
+        }
+
+        private void dreps_TextChanged(object sender, EventArgs e)
+        {
+            deadlift.reps = Convert.ToDouble(dreps.Text);
+        }
+
+        private void brweight_TextChanged(object sender, EventArgs e)
+        {
+            barbellrow.weight = Convert.ToDouble(brweight.Text);
+        }
+
+        private void brreps_TextChanged(object sender, EventArgs e)
+        {
+            barbellrow.reps = Convert.ToDouble(brreps.Text);
+        }
+
+        private void cweight_TextChanged(object sender, EventArgs e)
+        {
+            bicepcurl.weight = Convert.ToDouble(cweight.Text);
+        }
+
+        private void creps_TextChanged(object sender, EventArgs e)
+        {
+            bicepcurl.weight = Convert.ToDouble(creps.Text);
+        }
+
+        private void oweight_TextChanged(object sender, EventArgs e)
+        {
+            ohp.weight = Convert.ToDouble(oweight.Text);
+        }
+
+        private void oreps_TextChanged(object sender, EventArgs e)
+        {
+            ohp.reps = Convert.ToDouble(oreps.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bench.onerm = bench.weight + (bench.reps - 1) * 2.5;
+           
+            if (bench.onerm <= 0 )
+            {
+                bm.Text = "";
+            }
+            else
+            {
+                bm.Text = Convert.ToString(bench.onerm);
+            }
+            squat.onerm = squat.weight + (squat.reps - 1) * 5;
+            if(squat.onerm <= 0)
+            {
+                sm.Text = "";
+            }
+            else
+            {
+                sm.Text = Convert.ToString(squat.onerm);
+            }
+
+            deadlift.onerm = deadlift.weight + (deadlift.reps - 1) * 5;
+            if(deadlift.onerm <= 0)
+            {
+                dm.Text = "";
+            }
+            else
+            {
+                dm.Text = Convert.ToString(deadlift.onerm);
+            }
+
+            barbellrow.onerm = barbellrow.weight + (barbellrow.reps - 1) * 2.5;
+            if(barbellrow.onerm <= 0)
+            {
+                brm.Text = "";
+            }
+            else
+            {
+                brm.Text = Convert.ToString(barbellrow.onerm);
+            }
+
+            bicepcurl.onerm = bicepcurl.weight + (bicepcurl.reps - 1) * 2.5;
+            if(bicepcurl.onerm <= 0)
+            {
+                cm.Text = "";
+            }
+            else
+            {
+                cm.Text = Convert.ToString(bicepcurl.onerm);
+            }
+
+            ohp.onerm = ohp.weight + (ohp.reps - 1) * 1.25;
+            if(ohp.onerm <= 0)
+            {
+                om.Text = "";
+            }
+            else
+            {
+                om.Text = Convert.ToString(ohp.onerm);
+            }
+        }
+    }
+
+
+
+    class exercise
+    {
+        public string name;
+        public double weight;
+        public double reps;
+
+        public double onerm;
+
+
+        public exercise(string pname, double preps, double pweight, double ponerm)
+        {
+            name = pname;
+            reps = preps;
+            weight = pweight;
+            onerm = ponerm;
+
+
+
+        }
+
+        public string getname() { return name; }
+        public double getreps() { return reps; }
+        public double getweight() { return weight; }
+        public double getonerm() { return onerm; }
+    }
+
+
+
+
+
+
+
+}
+    
+
